09143fb01b7d56a38c26fc6d265b3039cc5f199a
Teslim <belloteslim645@gmail.com>
2024-09-24 15:16:17 +0100

    space

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,6 @@
+import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
 
 const HeroSection = () => {
   useEffect(() => {
@@ -12,31 +12,31 @@ const HeroSection = () => {
 
   return (
     <section
-      className="text-black  py-12 flex items-center justify-center "
+      className="text-black min-h-fit lg:min-h-screen flex items-center justify-center py-12"
       data-aos="fade-up"
       data-aos-delay="200"
     >
-      <div className="container mx-auto px-6 lg:px-12 text-center h-fit">
-        <h1 className="text-5xl lg:text-7xl font-bold leading-tight mb-6">
+      <div className="container mb-14 px-6 lg:px-12 text-center">
+        <h1 className="text-3xl sm:text-5xl lg:text-7xl font-bold leading-tight mb-4 sm:mb-6">
           Hi, I'm{" "}
           <span className="text-customBlue2 whitespace-nowrap">[ Teslim ]</span>
         </h1>
-        <p className="text-lg lg:text-xl font-light mb-12 max-w-3xl mx-auto">
+        <p className="text-base sm:text-lg lg:text-xl font-light mb-8 sm:mb-12 max-w-3xl mx-auto">
           A passionate web developer with expertise in creating modern,
           responsive websites and web apps using the latest technologies like
           React, Tailwind CSS, and more, with experience in UI design to ensure
           intuitive and user-friendly interfaces.
         </p>
-        <div className="flex justify-center gap-4">
+        <div className="flex flex-col sm:flex-row justify-center gap-4">
           <a
             href="#projects"
-            className="bg-customBlue2 text-black-600 px-8 py-3 rounded-full text-lg font-medium hover:bg-transparent border border-customBlue2 transition-all"
+            className="bg-customBlue2 text-black-600 px-6 sm:px-8 py-3 rounded-full text-base sm:text-lg font-medium hover:bg-transparent border border-customBlue2 transition-all"
           >
             View My Work
           </a>
           <a
             href="#contact"
-            className="bg-transparent border border-customBlue2 px-8 py-3 rounded-full text-lg font-medium text-customBlue2 hover:bg-customBlue2 hover:text-black transition-all"
+            className="bg-transparent border border-customBlue2 px-6 sm:px-8 py-3 rounded-full text-base sm:text-lg font-medium text-customBlue2 hover:bg-customBlue2 hover:text-black transition-all"
           >
             Get In Touch
           </a>
